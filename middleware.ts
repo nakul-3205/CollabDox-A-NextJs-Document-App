6bfac12f7fbf64ccb8761a57a6b7c288aee2adb2
nakul-3205 <nakulkejriwal079@gmail.com>
2025-07-26 14:12:42 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+    
+])
+export default clerkMiddleware(async (auth, req) => {
+  const {userId}=await auth()
+  if(!userId && !isPublicRoute(req)){
+    const url=new URL('/sign-up',req.url)
+    return NextResponse.redirect(url)
+  }
+  if(userId && isPublicRoute(req)){
+    const url=new URL('/dashboard',req.url)
+    return NextResponse.redirect(url)
+  }
+ 
+  
+
+})
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
